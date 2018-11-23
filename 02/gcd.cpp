348dddf9b1908871982a456184458910bcafc15e
Yaroslav Belgorosky <yshchukin@yandex.ru>
2018-11-23 19:24:41 +0300

    2.3: gcd solution was remastered

--- 02/gcd.cpp
+++ 02/gcd.cpp
@@ -1,23 +1,20 @@
 #include <cassert>
 #include <cstdint>
 #include <iostream>
+#include <algorithm>
 
 template <class Int>
 Int gcd(Int a, Int b)
 {
 	static_assert(std::is_integral<Int>::value, "a and b must be integral");
 
-	Int current_gcd = 1;
-	if (!a)
-		current_gcd = b;
-	else if (!b)
-		current_gcd = a;
-	else if (a <= b)
-		current_gcd = gcd(a, b % a);
-	else
-		current_gcd = gcd(a % b, b);
-
-	return current_gcd;
+    while (a && b)
+    {
+        a = a % b;
+        std::swap(a, b);
+    }
+    
+    return a ? a : b;
 }
 
 int main(void)
